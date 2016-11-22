d1a5d2141a501807781e92d0e193aea3bb0489ac
Hagar Yasser <hagar.y.omar@gmail.com>
2016-11-22 23:48:55 +0200

    walls model done :dancer:

---
+++ Shooter/Shooter/Walls.cpp
@@ -0,0 +1,9 @@
+//
+//  Walls.cpp
+//  Shooter
+//
+//  Created by Hagar Yasser Omar on 11/22/16.
+//  Copyright © 2016 Hagar Yasser Omar. All rights reserved.
+//
+
+#include "Walls.hpp"
---
+++ Shooter/Shooter/Walls.hpp
@@ -0,0 +1,14 @@
+//
+//  Walls.hpp
+//  Shooter
+//
+//  Created by Hagar Yasser Omar on 11/22/16.
+//  Copyright © 2016 Hagar Yasser Omar. All rights reserved.
+//
+
+#ifndef Walls_hpp
+#define Walls_hpp
+
+#include <stdio.h>
+
+#endif /* Walls_hpp */
